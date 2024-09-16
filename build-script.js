48500ba11b614bf134eba18662d26952260f7753
Vuthyy <141915285+Vuthyy@users.noreply.github.com>
2024-09-16 12:46:18 +0700

    Vers37

--- build-script.js
+++ build-script.js
@@ -8,70 +8,64 @@ esbuild
     entryPoints: ["src/server.ts"],
     bundle: true,
     platform: "node",
-    target: "node20",
+    target: "node20", // Target depends on your environment
     outdir: "build",
-    external: ["express"],
+    external: ["express"], // Specify Node.js packages here
     loader: {
       ".ts": "ts",
     },
     plugins: [
+      // (2) Solve: https://stackoverflow.com/questions/62136515/swagger-ui-express-plugin-issue-with-webpack-bundling-in-production-mode/63048697#63048697
       copy({
-        assets: {
-          from: [
-            "./node_modules/swagger-ui-dist/*.css",
-            "./node_modules/swagger-ui-dist/*.js",
-            "./node_modules/swagger-ui-dist/*.png",
-          ],
-          to: ["./"],
-        },
+        assets: [
+          {
+            from: `../node_modules/swagger-ui-dist/*.css`,
+            to: "./",
+          },
+          {
+            from: `../node_modules/swagger-ui-dist/*.js`,
+            to: "./",
+          },
+          {
+            from: `../node_modules/swagger-ui-dist/*.png`,
+            to: "./",
+          },
+          {
+            from: "./src/configs/.env.production",
+            to: "./configs",
+          },
+        ],
       }),
-      {
-        name: "log-success-after-copy",
-        setup(build) {
-          build.onEnd(() => {
-            console.log("Assets copied successfully");
-          });
-        },
-      },
     ],
     resolveExtensions: [".ts", ".js"],
     define: {
       "process.env.NODE_ENV": '"production"',
     },
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "."),
     },
   })
   .then(() => {
-    // Correctly handle file copying
+    // (1) Solve: Copy swagger.json after successful build
     fs.copySync(
       path.resolve(__dirname, "src/docs/swagger.json"),
       path.resolve(__dirname, "build/docs/swagger.json")
     );
     console.log("Swagger JSON copied successfully!");
 
-    fs.copySync(
-      path.resolve(__dirname, "ecosystem.config.js"),
-      path.resolve(__dirname, "build/ecosystem.config.js")
-    );
-    console.log("Ecosystem Config copied successfully!");
-
-    const envFilePath = path.resolve(__dirname, "src/configs/.env.development");
-    if (fs.existsSync(envFilePath)) {
-      fs.copySync(
-        envFilePath,
-        path.resolve(__dirname, "build/configs/.env.production")
-      );
-      console.log("Environment file copied successfully!");
-    } else {
-      console.error("Environment file does not exist:", envFilePath);
-    }
-
+    // Copy package.json after ensuring the build was successful
     fs.copySync(
       path.resolve(__dirname, "package.json"),
       path.resolve(__dirname, "build/package.json")
     );
     console.log("Package.json copied successfully!");
+
+    // Copy ecosystem.config.js after ensuring the build was successful
+    fs.copySync(
+      path.resolve(__dirname, "ecosystem.config.js"),
+      path.resolve(__dirname, "build/ecosystem.config.js")
+    );
+    console.log("Ecosystem Config copied successfully!");
   })
   .catch((error) => {
     console.error("Build failed:", error);
